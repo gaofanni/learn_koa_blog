4abc1a239d41a399ba0628dbb3bb617f3357a113
gaofanni <gaofanni@4399inc.com>
2017-11-13 19:00:55 +0800

    commit

---
+++ config/default.js
@@ -0,0 +1,11 @@
+const config = {
+    port: 4000,
+    database: {
+        DATABASE: 'nodesql',
+        USERNAME: 'root',
+        PASSWORD: '123456',
+        PORT: '4040',
+        HOST: 'localhost'
+    }
+}
+module.exports = config
---
+++ index.js
@@ -0,0 +1,47 @@
+const Koa = require('koa')
+const path = require('path')
+const bodyParser = require('koa-bodyparser')
+const ejs = requre('ejs')
+const session = requre('koa-session-minimal')
+const MysqlStore = require('koa-mysql-session')
+const config = require('./config/default')
+const router = require('koa-router')
+const views=require('koa-views')
+const koaStatic = require('koa-static')
+const app = new Koa()
+
+//session存储配置
+const sessionMysqlConfig = {
+    user:config.database.USERNAME,
+    password:config.database.PASSWORD,
+    database:config.database.DATABASE,
+    host:config.database.HOST
+}
+
+//配置session中间件
+app.use(session({
+    key:'USER_SID',
+    store:new MysqlStore(sessionMysqlConfig)
+}))
+
+//配置静态资源加载中间件
+app.use(koaStatic(
+    path.join(__dirname,'./public')
+))
+
+//配置服务端模板渲染引擎中间件
+app.use(views(
+    path.join(__dirname,'./views'),{
+        extension:'ejs'
+    }
+))
+
+//表单解析中间件
+app.use(bodyParser())
+
+//配置路由
+
+//监听端口
+app.listen(config.port)
+
+console.log('listening on port'+config.port)
